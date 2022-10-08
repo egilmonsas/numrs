1459471919cb468ea9ae7ef9d2a4c1b9c7f2ef64
Vasse <egilmonsas@gmail.com>
2022-10-08 22:29:57 +0200

    improved readability

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,18 @@ pub mod calculus;
 pub mod eq_solve;
 pub mod ode;
 pub mod utils;
+use std::fmt;
 
+#[derive(Clone, Copy)]
 pub struct Vec2 {
     x: F,
     y: F,
 }
-
+impl fmt::Display for Vec2 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "x: {:.2} y: {:.2}", self.x, self.y)
+    }
+}
 type F = f64;
 pub const EPSILON: F = 0.0000001;
 pub const E: F = std::f64::consts::E;
--- src/ode.rs
+++ src/ode.rs
@@ -5,36 +5,50 @@ pub enum OdeMethod {
     Naive,
 }
 
-pub fn compute_next(method: OdeMethod, dy: &dyn Fn(&Vec2) -> f64, p0: Vec2, h: F) -> Vec2 {
+pub fn step_forward<FN: Fn(Vec2) -> f64>(method: OdeMethod, dy: &FN, p0: Vec2, h: F) -> Vec2 {
     match method {
         OdeMethod::Rk4 => rk4(dy, p0, h),
         OdeMethod::Naive => naive(dy, p0, h),
     }
 }
 
-fn naive(dy: &dyn Fn(&Vec2) -> f64, p0: Vec2, h: F) -> Vec2 {
+fn naive<FN: Fn(Vec2) -> f64>(dy: &FN, p0: Vec2, h: F) -> Vec2 {
     Vec2 {
         x: p0.x + h,
-        y: p0.y + h * dy(&p0),
+        y: p0.y + h * dy(p0),
     }
 }
 
-fn rk4(dy: &dyn Fn(&Vec2) -> f64, p0: Vec2, h: F) -> Vec2 {
-    let k1 = dy(&p0);
-    let k2 = dy(&Vec2 {
+fn rk4<FN: Fn(Vec2) -> f64>(dy: &FN, p0: Vec2, h: F) -> Vec2 {
+    let k1 = dy(p0);
+    let k2 = dy(Vec2 {
         x: p0.x + h / 2.0,
         y: p0.y + h * k1 / 2.,
     });
-    let k3 = dy(&Vec2 {
+    let k3 = dy(Vec2 {
         x: p0.x + h / 2.0,
         y: p0.y + h * k2 / 2.0,
     });
-    let k4 = dy(&Vec2 {
+    let k4 = dy(Vec2 {
         x: p0.x + h,
         y: p0.y + h * k3,
     });
     Vec2 {
         x: p0.x + h,
         y: p0.y + (k1 + (k2 + k3) * 2.0 + k4) / 6.0 * h,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rk4() {
+        let p0 = Vec2 { x: 0.0, y: 1.0 };
+        let h = 0.5;
+        let dy = |p: Vec2| p.x.sin().powi(2) * p.y;
+        let res = step_forward(OdeMethod::Rk4, &dy, p0, h);
+        println!("{}", res)
     }
 }
