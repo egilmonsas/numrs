6e5ca90ef7bba56a220bbcb2baa3b23a5b442482
Vasse <egilmonsas@gmai.com>
2022-12-18 12:35:13 +0100

    added basic rsarray

---
+++ benches/sorting_benchmark.rs
@@ -0,0 +1,37 @@
+use mathrs::rsarray::*;
+use rand;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn bench_sum(c: &mut Criterion) {
+    fn f(v: Vec<f64>) -> f64 {
+        let a = rsarray { items: v };
+        a.sum()
+    }
+    let arr: Vec<f64> = black_box((0..10000).map(|_| rand::random::<f64>()).collect());
+
+    c.bench_function("min()", |b| b.iter(|| f(arr.clone())));
+}
+
+fn bench_min(c: &mut Criterion) {
+    fn f(v: Vec<f64>) -> f64 {
+        let a = rsarray { items: v };
+        a.min()
+    }
+    let arr: Vec<f64> = black_box((0..10000).map(|_| rand::random::<f64>()).collect());
+
+    c.bench_function("sorting algorithm", |b| b.iter(|| f(arr.clone())));
+}
+
+fn bench_max(c: &mut Criterion) {
+    fn f(v: Vec<f64>) -> f64 {
+        let a = rsarray { items: v };
+        a.max()
+    }
+    let arr: Vec<f64> = black_box((0..10000).map(|_| rand::random::<f64>()).collect());
+
+    c.bench_function("max()", |b| b.iter(|| f(arr.clone())));
+}
+
+criterion_group!(benches, bench_min, bench_max);
+criterion_main!(benches);
